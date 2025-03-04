4dba6cfdb7cbd972d370bf1afa0733c2e141418c
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-04 12:23:21 -0300

    BCryptPassword Encoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$FAPGXA7FdCKv2IVGcieQ5u8mUlw2B5SSEsMPBxsWz77acSilyzAMe');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$FAPGXA7FdCKv2IVGcieQ5u8mUlw2B5SSEsMPBxsWz77acSilyzAMe');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
