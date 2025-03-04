c94d80c5ec0000e8457c79dfb8ccc50832a16a6e
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-04 15:47:27 -0300

    Product update

--- src/main/java/com/devsuperior/demo/entities/Product.java
+++ src/main/java/com/devsuperior/demo/entities/Product.java
@@ -1,10 +1,18 @@
 package com.devsuperior.demo.entities;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,21 +23,32 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TEXT")
-    private String description;
-    private Double price;
-    private String imgUrl;
-    
-    public Product() {
-    }
-	
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
+	private String description;
+	private Double price;
+	private String imgUrl;
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
+	public Product() {
+	}
+
+	public Product(Long id, String name, String description, Double price, String imgUrl, Set<Category> categories,
+			Set<OrderItem> items) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
-		this.setPrice(price);
-		this.setImgUrl(imgUrl);
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.categories = categories;
+		this.items = items;
 	}
 
 	public Long getId() {
@@ -39,13 +58,21 @@ public class Product {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Double getPrice() {
@@ -63,4 +90,41 @@ public class Product {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<OrderItem> items) {
+		this.items = items;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categories, description, id, imgUrl, items, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(categories, other.categories) && Objects.equals(description, other.description)
+				&& Objects.equals(id, other.id) && Objects.equals(imgUrl, other.imgUrl)
+				&& Objects.equals(items, other.items) && Objects.equals(name, other.name)
+				&& Objects.equals(price, other.price);
+	}
+
 }
