463846b77983a7e184f3e115aa34499f18612769
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-04 15:55:03 -0300

    Validation dependency, CategoryDTO, productDTO update

--- src/main/java/com/devsuperior/demo/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/demo/dto/ProductDTO.java
@@ -1,11 +1,32 @@
 package com.devsuperior.demo.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.devsuperior.demo.entities.Product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
 	private Long id;
-	private String name;
+    @Size(min = 3, max = 80, message = "Nome precisar ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String description;
+    @NotNull(message = "Campo requerido")
+    @Positive(message = "O preço deve ser positivo")
+    private Double price;
+    private String imgUrl;
+    
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 	
 	public ProductDTO() {
 	}
@@ -26,5 +47,13 @@ public class ProductDTO {
 
 	public String getName() {
 		return name;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 }
