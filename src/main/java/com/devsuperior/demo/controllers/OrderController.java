5786fc3dc60f7f5d7593cc77ea69bc109fc9dba8
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-04 23:15:57 -0300

    DTOs done

--- src/main/java/com/devsuperior/demo/controllers/OrderController.java
+++ src/main/java/com/devsuperior/demo/controllers/OrderController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devsuperior.demo.dto.OrderDTO;
+
 import jakarta.validation.Valid;
 
 @RestController
